1165ab79653123da13da8fd7782e435f14826e90
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2019-10-23 22:11:48 +0900

    シーンのリンク修正

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,6 +2,128 @@
 
 let panorama;
 
+// const coordArr = [{
+//   x: 0,
+//   y: 3,
+//   z: 0,
+// }, {
+//   x: 15,
+//   y: 3,
+//   z: 0,
+// }, {
+//   x: 15,
+//   y: 3,
+//   z: -15,
+// }, {
+//   x: 15,
+//   y: 3,
+//   z: -30,
+// }, {
+//   x: 30,
+//   y: 3,
+//   z: -30,
+// }, {
+//   x: 50,
+//   y: 3,
+//   z: -30,
+// }, {
+//   x: 50,
+//   y: 3,
+//   z: -15,
+// }, {
+//   x: 50,
+//   y: 3,
+//   z: 0,
+// }, {
+//   x: 65,
+//   y: 3,
+//   z: 0,
+// }, {
+//   x: 80,
+//   y: 3,
+//   z: 0,
+// }, {
+//   x: 80,
+//   y: 3,
+//   z: 20,
+// }];
+
+linkArr = [
+  [{
+    heading: 90,
+    description: 'Forward',
+    pano: `pano1`,
+  }],
+  [{
+    heading: 180,
+    description: 'Forward',
+    pano: `pano2`,
+  }, {
+    heading: 270,
+    description: 'Backward',
+    pano: `pano0`,
+  }],
+  [{
+    heading: 180,
+    description: 'Forward',
+    pano: `pano3`,
+  }, {
+    heading: 0,
+    description: 'Backward',
+    pano: `pano1`,
+  }],
+  [{
+    heading: 90,
+    description: 'Forward',
+    pano: `pano4`,
+  }, {
+    heading: 0,
+    description: 'Backward',
+    pano: `pano2`,
+  }],
+  [{
+    heading: 90,
+    description: 'Forward',
+    pano: `pano5`,
+  }, {
+    heading: 270,
+    description: 'Backward',
+    pano: `pano3`,
+  }],
+  [{
+    heading: 0,
+    description: 'Forward',
+    pano: `pano6`,
+  }, {
+    heading: 270,
+    description: 'Backward',
+    pano: `pano4`,
+  }],
+  [{
+    heading: 0,
+    description: 'Forward',
+    pano: `pano7`,
+  }, {
+    heading: 180,
+    description: 'Backward',
+    pano: `pano5`,
+  }],
+  [{
+    heading: 90,
+    description: 'Forward',
+    pano: `pano8`,
+  }, {
+    heading: 180,
+    description: 'Backward',
+    pano: `pano6`,
+  }],
+  [{
+    heading: 270,
+    description: 'Backward',
+    pano: `pano7`,
+  }],
+];
+
 // StreetViewPanoramaData of a panorama just outside the Google Sydney office.
 let outsideGoogle;
 
@@ -11,15 +133,7 @@ const getReceptionPanoramaData = n => ({
     pano: `pano${n}`,  // The ID for this custom panorama.
     description: `Panorama ${n}`,
   },
-  links: [{
-    heading: 0,
-    description: 'Forward',
-    pano: `pano${n + 1}`
-  }, {
-    heading: 180,
-    description: 'Backward',
-    pano: `pano${n - 1}`
-  }],
+  links: linkArr[n],
   copyright: 'Imagery (c) 2019 IWABUCHI Yu(u)ki butchi',
   tiles: {
     tileSize: new google.maps.Size(1024, 512),
